a460e0d685128842a741c3955a20d7867bb3545d
dustinwaldron <waldrd11@gmail.com>
2019-07-08 00:02:15 -0400

    add auth check for edit route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -19,7 +22,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
@@ -38,5 +40,17 @@ class UsersController < ApplicationController
       params.require(:user).permit(
         :name, :email, :password, :password_confirmation
       )
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
